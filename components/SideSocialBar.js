45f690cfd3d320f32db682d57b9a4ab260a92355
BradleyPekeur <bradley@duroplastic.com>
2022-09-09 08:26:55 +0200

    Image Paths Hosting Changed

--- components/SideSocialBar.js
+++ components/SideSocialBar.js
@@ -5,9 +5,9 @@ function SideSocialBar() {
   return (
     <div className='hidden md:flex h-[80vh] border-r-4 border-white-500 fixed  z-50 flex-col w-[80px] mt-24'>
       <div className=' w-full h-full relative flex flex-col items-center'>
-        <div className='absolute bottom-12'><Image  src="/facebook.svg" alt="Facebook Button"  width={45} height={45}/></div>
-        <div className='absolute bottom-24'><Image src="/twitter.svg" alt="Twitter Button"  width={45} height={45}/></div>
-        <div className='absolute bottom-36'><Image src="/linkedin.svg" alt="Linkedin Button"  width={45} height={45}/></div>
+        <div className='absolute bottom-12'><Image  src="http://usembleit.co.za/img/social/Facebook.svg" alt="Facebook Button"  width={45} height={45}/></div>
+        <div className='absolute bottom-24'><Image src="http://usembleit.co.za/img/social/Twitter.svg" alt="Twitter Button"  width={45} height={45}/></div>
+        <div className='absolute bottom-36'><Image src="http://usembleit.co.za/img/social/LinkedIn.svg" alt="Linkedin Button"  width={45} height={45}/></div>
       </div>
     </div>
   )
