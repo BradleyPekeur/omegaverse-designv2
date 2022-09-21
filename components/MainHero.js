209fa2757212b5848335a68c76f5ca5d40023c4f
BradleyPekeur <bradley@duroplastic.com>
2022-09-21 09:31:23 +0200

    temp changed hero untill better video is rendered

--- components/MainHero.js
+++ components/MainHero.js
@@ -1,39 +1,44 @@
-
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap/dist/gsap";
-import Image from 'next/image';
-import VideoScroll from './VideoScroll';
-import MobileVideoScroll from './MobileVideoScroll';
+import Image from "next/image";
+import VideoScroll from "./VideoScroll";
+import MobileVideoScroll from "./MobileVideoScroll";
 
 function MainHero() {
-    const heroSection = useRef(null)
-    const heroSection2 = useRef(null)
-    const heroText  = useRef(null)
-    const videoIntro  = useRef(null)
+	const heroSection = useRef(null);
+	const heroSection2 = useRef(null);
+	const heroText = useRef(null);
+	const videoIntro = useRef(null);
 
-    const tl = useRef();
-    const transformer = useRef(null)
+	const tl = useRef();
+	const transformer = useRef(null);
 
-    useEffect(()=>{ 
+	useEffect(() => {
+		tl.current = gsap
+			.timeline()
+			// .fromTo(videoIntro.current, {opacity:1 }, {opacity:1,delay:0, duration:6})
+			// .fromTo(heroSection.current, {display:"block" }, {display:"none", delay:0,duration:3},"<")
 
-      tl.current = gsap.timeline()  
-      // .fromTo(videoIntro.current, {opacity:1 }, {opacity:1,delay:0, duration:6}) 
-      // .fromTo(heroSection.current, {display:"block" }, {display:"none", delay:0,duration:3},"<")
-      
-       
-      .fromTo(transformer.current, {scale:1,  }, {scale:1.1,duration:5, ease:"elastic.out(0.9, 0.1)"  })
-      .fromTo(heroText.current, {x:"-100%",  }, {x:0,scale:1.1,duration:0.75, ease:"elastic.out(0.9, 0.1)"}, "<")
+			.fromTo(
+				transformer.current,
+				{ scale: 1 },
+				{ scale: 1.1, duration: 5, ease: "elastic.out(0.9, 0.1)" }
+			)
+			.fromTo(
+				heroText.current,
+				{ x: "-100%" },
+				{ x: 0, scale: 1.1, duration: 0.75, ease: "elastic.out(0.9, 0.1)" },
+				"<"
+			);
 
-      window.onload = function()
-{
-  tl.play();
-}
+		window.onload = function () {
+			tl.play();
+		};
+	}, []);
 
-    },[])
-
-  return (
-    <>
-    {/* <div className='pt-24 relative overflow-hidden min-h-[100vh] intro-hero' 
+	return (
+		<>
+			{/* <div className='pt-24 relative overflow-hidden min-h-[100vh] intro-hero' 
     ref={heroSection}
 
     >   
@@ -42,24 +47,34 @@ function MainHero() {
         ></video>
          
     </div> */}
-    <div className="hidden md:flex"><VideoScroll /></div>
-    <div className="flex md:hidden"><MobileVideoScroll /></div>
+			{/* <div className="hidden md:flex"><VideoScroll /></div>
+    <div className="flex md:hidden"><MobileVideoScroll /></div> */}
 
-    <div className='min-h-[100vh] bg-[#010101]  flex justify-center items-center'
-    ref={heroSection2}>
-      <div className='max-w-[80vw] flex items-center'>
-        <div ref={heroText}>
-          <h1 className='heading  text-white w-full'>High Precision Engineering</h1>
-          <p className='pText text-[#FF6600]'>Africa’s larges milling and 3D Printing machine</p>
-        </div>
-        <div ref={transformer}>
-          <Image src="/img/home/Transform.png" alt="Twitter Button"  width={800} height={1028}/>           
-        </div>
-      </div>
-    </div>
-    
-    </>
-  )
+			<div
+				className="min-h-[100vh] bg-[#010101]  flex  justify-center items-center"
+				ref={heroSection2}
+			>
+				<div className="max-w-[80vw] flex flex-col-reverse md:flex-row items-center">
+					<div ref={heroText}>
+						<h1 className="heading  text-white w-full">
+							High Precision Engineering
+						</h1>
+						<p className="pText text-[#FF6600]">
+							Africa’s larges milling and 3D Printing machine
+						</p>
+					</div>
+					<div ref={transformer}>
+						<img
+							src="/img/home/OmegaCharacter.png"
+							alt="Twitter Button"
+							width={800}
+							height={1028}
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default MainHero
+export default MainHero;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,41 +1,88 @@
-import React, { useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { FaHamburger } from 'react-icons/fa';
-import { GiSplitCross } from 'react-icons/gi';
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { FaHamburger } from "react-icons/fa";
+import { GiSplitCross } from "react-icons/gi";
 
 function Navbar() {
+	const [nav, setNav] = useState(false);
 
-  const [nav, setNav] = useState(false);
-  
-  const handleNav = () => {
-      setNav(!nav)
-  };  
+	const handleNav = () => {
+		setNav(!nav);
+	};
 
-  return (
-    <nav className='navbarBG navbg flex justify-around w-screen fixed pt-14 pb-14 mb-10 px-[20px] md:px-[170px] z-50'>
-      <Link href="./"><div className='min-h-[40px]'><Image src="/omegaverse_logo.svg" alt="Logo"  width={200} height={40} /></div></Link>
-      <ul className='hidden md:flex space-x-4 w-full justify-center text-white text-size-4' >
-        <li><Link href="./">Home</Link></li>
-        <li><Link href="./endeavor">Endeavor</Link></li>
-        <li><Link href="./services">Services</Link></li>
-        <li><Link href="./team">Team</Link></li>
-        <li><Link href="./contact">Contact Us</Link></li>        
-      </ul>
-      <button className='bg-[#FF6600]  px-1 rounded-full w-[200px] hidden md:flex justify-center items-center'>Store</button>
-      <div onClick={handleNav} className='block md:hidden'>
-        {nav ? <GiSplitCross size={20} style={{color:"white"}}/> : <FaHamburger size={20} style={{color:"white"}} />}
-      </div>
-      <ul  className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 text-[#FF6600] pl-[90px] pt-[60px]' : 'ease-in-out duration-500 fixed left-[-100%] '}>
-      <div className='min-h-[40px]'><Image src="/omegaverse_logo.svg" alt="Logo"  width={200} height={40} /></div>
-          <li className='p-4 border-b border-gray-600'><Link href="./">Home</Link></li>
-          <li className='p-4 border-b border-gray-600'><Link href="./endeavor">Endeavor</Link></li>
-          <li className='p-4 border-b border-gray-600'><Link href="./services">Services</Link></li>
-          <li className='p-4 border-b border-gray-600'><Link href="./team">Team</Link></li>
-          <li className='p-4'><Link href="./contact">Contact Us</Link></li>
-      </ul>
-    </nav>
-  )
+	return (
+		<nav className="navbarBG navbg flex justify-around w-screen fixed pt-14 pb-14 mb-10 px-[20px] md:px-[170px] z-50">
+			<Link href="./">
+				<div className="min-h-[40px]">
+					<Image
+						src="/omegaverse_logo.svg"
+						alt="Logo"
+						width={200}
+						height={40}
+					/>
+				</div>
+			</Link>
+			<ul className="hidden md:flex space-x-4 w-full justify-center text-white text-size-4">
+				<li>
+					<Link href="./">Home</Link>
+				</li>
+				<li>
+					<Link href="./endeavor">Endeavor</Link>
+				</li>
+				<li>
+					<Link href="./services">Services</Link>
+				</li>
+				<li>
+					<Link href="./team">Team</Link>
+				</li>
+				<li>
+					<Link href="./contact">Contact Us</Link>
+				</li>
+			</ul>
+			<button className="bg-[#FF6600]  px-1 rounded-full w-[200px] hidden md:flex justify-center items-center">
+				Store
+			</button>
+			<div onClick={handleNav} className="block md:hidden z-50">
+				{nav ? (
+					<GiSplitCross size={20} style={{ color: "white" }} />
+				) : (
+					<FaHamburger size={20} style={{ color: "white" }} />
+				)}
+			</div>
+			<ul
+				className={
+					nav
+						? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 text-[#FF6600] pl-[90px] pt-[60px]"
+						: "ease-in-out duration-500 fixed left-[-100%] "
+				}
+			>
+				<div className="min-h-[40px]">
+					<Image
+						src="/omegaverse_logo.svg"
+						alt="Logo"
+						width={200}
+						height={40}
+					/>
+				</div>
+				<li className="p-4 border-b border-gray-600">
+					<Link href="./">Home</Link>
+				</li>
+				<li className="p-4 border-b border-gray-600">
+					<Link href="./endeavor">Endeavor</Link>
+				</li>
+				<li className="p-4 border-b border-gray-600">
+					<Link href="./services">Services</Link>
+				</li>
+				<li className="p-4 border-b border-gray-600">
+					<Link href="./team">Team</Link>
+				</li>
+				<li className="p-4">
+					<Link href="./contact">Contact Us</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
-export default Navbar
+export default Navbar;
